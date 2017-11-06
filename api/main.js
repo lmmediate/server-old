4985e2bee721aee3349db7055c49cc304209dba5
Maxim Surovtsev <realitybelieves@gmail.com>
2017-11-06 14:42:48 +0300

    Express static test.

--- api/main.js
+++ api/main.js
@@ -1,14 +1,10 @@
 var express = require('express');
-var fs = require('fs');
 
 var app = express();
 
-app.get('/', function(req, res) {
-  console.log('GET');
-  res.status(200).json({ name: 'Maxim' });
-});
+app.use(express.static('../../web-app'));
 
 var port = 4000;
 app.listen(port, function() {
   console.log('Listening on port ' + port);
-})
+});
