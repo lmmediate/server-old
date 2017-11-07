d4024f0f8aa0cb23870e360c97cac02b26b3b2b2
Maxim Surovtsev <realitybelieves@gmail.com>
2017-11-07 16:12:00 +0300

    Reading JSON file from crawler.

--- api/main.js
+++ api/main.js
@@ -4,7 +4,12 @@ var app = express();
 
 app.use(express.static('../../web-app'));
 
-var port = 4000;
+app.get('/api/sales', function (req, res) {
+  var data = require('../crawler/out/sales'); // File extension is optional
+  res.json(data);
+});
+
+var port = 8080;
 app.listen(port, function() {
   console.log('Listening on port ' + port);
 });
