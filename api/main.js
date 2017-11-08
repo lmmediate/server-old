82da5ddc2e94d94c953067410fc27967b165292c
Maxim Surovtsev <realitybelieves@gmail.com>
2017-11-08 12:54:37 +0300

    Moved router logic to separate module.

--- api/main.js
+++ api/main.js
@@ -1,35 +1,8 @@
 var express = require('express');
 
 var app = express();
-
+app.use('/api', require('./routes/api'));
 app.use(express.static('../../web-app'));
-
-
-var dataPath = '../crawler/out/sales'; // File extension is optional
-
-app.get('/api/sales', function(req, res) {
-  console.log(req.query);
-
-  var data = require(dataPath);
-  if(req.query.name) {
-      data = data.filter(function(value) {
-      return value.name.toLowerCase().indexOf(req.query.name.toLowerCase()) !== -1;
-    });
-  } 
-
-  res.json(data);
-});
-
-app.get('/api/sales/categories', function (req, res) {
-  var data = require(dataPath);
-  data = data.map(function(value, index, arr) { 
-    return value.category;
-  }).filter(function (value, index, arr) { 
-    return arr.indexOf(value) === index;
-  });
-  
-  res.json(data);
-});
 
 var port = 8080;
 app.listen(port, function() {
---
+++ api/routes/api.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+
+var dataPath = '../../crawler/out/sales'; // File extension is optional
+
+router.get('/sales', function(req, res) {
+  console.log(req.query);
+
+  var data = require(dataPath);
+  if(req.query.name) {
+      data = data.filter(function(value) {
+      return value.name.toLowerCase().indexOf(req.query.name.toLowerCase()) !== -1;
+    });
+  } 
+
+  res.json(data);
+});
+
+router.get('/sales/categories', function (req, res) {
+  var data = require(dataPath);
+  data = data.map(function(value, index, arr) { 
+    return value.category;
+  }).filter(function (value, index, arr) { 
+    return arr.indexOf(value) === index;
+  });
+  
+  res.json(data);
+});
+
+
+module.exports = router;
