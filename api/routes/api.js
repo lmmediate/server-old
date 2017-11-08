de8e70c170c034bd928f172be46fc6bb934d2443
Maxim Surovtsev <realitybelieves@gmail.com>
2017-11-08 22:45:48 +0300

    Simple pagination.

--- api/routes/api.js
+++ api/routes/api.js
@@ -3,6 +3,11 @@ var router = express.Router();
 
 
 var dataPath = '../../crawler/out/sales'; // File extension is optional
+var itemsPerPage = 30;
+// TODO: maybe data should be stored in global variable?
+// and for example call setInterval
+// var data = require(dataPath);
+
 
 router.get('/sales', function(req, res) {
   console.log(req.query);
@@ -30,9 +35,23 @@ router.get('/sales/categories', function (req, res) {
 
 router.get('/sales/info', function (req, res) {
   var data = require(dataPath);
+
   var count = data.length;
-  var numPages = Math.ceil(count / 30);
-  res.json({ count: count, numPages: numPages });
+  var numPages = Math.ceil(count / itemsPerPage);
+  
+  res.json({ count: count, numPages: numPages, itemsPerPage: itemsPerPage });
+});
+
+router.get('/sales/page/:num', function (req, res) {
+  var data = require(dataPath);
+  var num = req.params.num;
+
+  // TODO: type and negative bound check
+
+  console.log(num);
+  data = data.slice(itemsPerPage * (num - 1), itemsPerPage * num);
+
+  res.json(data);
 });
 
 
