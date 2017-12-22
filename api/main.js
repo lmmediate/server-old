1788b81ecf7710a9e30eec1bec5309b96e89d47f
Maxim Surovtsev <realitybelieves@gmail.com>
2017-12-22 18:50:30 +0300

    Set SPA routes.

--- api/main.js
+++ api/main.js
@@ -1,8 +1,15 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
 app.use('/api', require('./routes/api'));
 app.use(express.static('../../web-app'));
+
+let handler = (req, res) => res.sendFile(path.join(__dirname, "../../web-app/index.html"));
+
+let routes = ["/", "/discounts", "/discounts/:num", "/shoplist"];
+
+routes.forEach(route => app.get(route, handler));
 
 var port = 8080;
 app.listen(port, function() {
