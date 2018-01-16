1813614a821bfa3e56cae5272d4331c938fb0264
Maxim Surovtsev <realitybelieves@gmail.com>
2018-01-16 09:11:01 +0300

    Repository annotation.

--- src/main/java/ru/easysales/server/ItemRepository.java
+++ src/main/java/ru/easysales/server/ItemRepository.java
@@ -1,6 +1,8 @@
 package ru.easysales.server;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemRepository extends CrudRepository<Item, Integer> {
 }
