b056249cceb94830eac07f588a1de4516f121511
Maxim Surovtsev <realitybelieves@gmail.com>
2018-01-17 19:23:17 +0300

    Get current categories.

--- src/main/java/ru/easysales/server/controller/ItemController.java
+++ src/main/java/ru/easysales/server/controller/ItemController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.*;
 import ru.easysales.server.entity.Item;
 import ru.easysales.server.service.ItemService;
 
+import java.util.Set;
+
 @RestController
 @RequestMapping("/api")
 public class ItemController {
@@ -17,13 +19,19 @@ public class ItemController {
         return itemService.getCurrentItems();
     }
 
-    @GetMapping("/sales/info")
-    public Object getInfo() {
-        return itemService.getInfo();
+    @GetMapping("/sales/categories")
+    public Set<String> getCurrentCategories() {
+        return itemService.getCurrentCategories();
     }
 
     @PostMapping("/sales")
     public Item addItem(@RequestBody Item item) {
         return itemService.addItem(item);
     }
+
+    @GetMapping("/sales/info")
+    public Object getInfo() {
+        return itemService.getInfo();
+    }
 }
+
--- src/main/java/ru/easysales/server/service/ItemService.java
+++ src/main/java/ru/easysales/server/service/ItemService.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Service;
 import ru.easysales.server.dao.ItemDao;
 import ru.easysales.server.entity.Item;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class ItemService {
@@ -15,6 +17,15 @@ public class ItemService {
 
     public List<Item> getCurrentItems() {
         return itemDao.getCurrentItems();
+    }
+
+    public Set<String> getCurrentCategories() {
+        Set<String> set = new HashSet<>();
+        List<Item> items = getCurrentItems();
+        for (Item item : items) {
+            set.add(item.getCategory());
+        }
+        return set;
     }
 
     public Item addItem(Item item) {
