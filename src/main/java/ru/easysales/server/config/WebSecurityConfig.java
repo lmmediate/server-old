7653ec563f6985f5601be19d478e03785c3635a0
Maxim Surovtsev <realitybelieves@gmail.com>
2018-02-11 17:02:36 +0300

    Allow some requests.

--- src/main/java/ru/easysales/server/config/WebSecurityConfig.java
+++ src/main/java/ru/easysales/server/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package ru.easysales.server.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,13 +10,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/", "/dist/**", "/api/sales/**")
+                .permitAll()
                 .anyRequest()
-                .fullyAuthenticated()
-                .and()
-                .httpBasic()
-                .and()
-                .csrf().disable();
+                .fullyAuthenticated();
+//                .and()
+//                .httpBasic()
+//                .and()
+//                .csrf().disable();
     }
 }
 
